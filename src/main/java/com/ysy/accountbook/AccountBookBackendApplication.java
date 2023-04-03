15e23f1a1defe8700d6b5a5cd7d941bd3e499d9a
yenow <phantom_ysy@naver.com>
2023-04-03 18:41:28 +0900

    project start

---
+++ src/main/java/com/ysy/accountbook/AccountBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ysy.accountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ysy/accountbook/AccountBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.accountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
